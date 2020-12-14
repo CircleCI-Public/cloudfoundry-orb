e2556cd433064415e1d62d6425a2374ca4cbcd81
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-12-14 13:42:55 +0000

    ~ -> $HOME

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -5,9 +5,9 @@ curl -L $BASE_INSTALLER_URL | tar -zx
 if which sudo > /dev/null; then
 	sudo mv cf /usr/local/bin
 else
-	mkdir -p ~/bin
-	mv cf ~/bin
-	echo "export PATH=\"${PATH}:~/bin\"" >> $BASH_ENV
+	mkdir -p $HOME/bin
+	mv cf $HOME/bin
+	echo "export PATH=\"${PATH}:${HOME}/bin\"" >> $BASH_ENV
 fi
 
 cf version
