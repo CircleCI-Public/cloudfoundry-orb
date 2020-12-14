08045a3c99138fbe58255d996de7df84be601475
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-12-14 11:22:35 +0000

    Split auth into own command.

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,12 +1,5 @@
-: "${CF_USERNAME?Cloud Foundry username and password must be set as Environment variables before running this command.}"
+BASE_INSTALLER_URL="https://packages.cloudfoundry.org/stable?release=$CLOUDFOUNDRY_BINARY_PLATFORM-binary&source=github&version=v$CLOUDFOUNDRY_BINARY_PLATFORM"
+curl -L $BASE_INSTALLER_URL | tar -zx
+mv cf /usr/local/bin
 
-: "${CF_PASSWORD?Cloud Foundry username and password must be set as Environment variables before running this command.}"
-
-curl -v -L -o cf-cli_amd64.deb "https://cli.run.pivotal.io/stable?release=debian64&source=github"
-
-sudo dpkg -i cf-cli_amd64.deb
-
-cf -v
-cf api "$INSTALL_ENDPOINT"
-cf auth "$CF_USERNAME" "$CF_PASSWORD"
-cf target -o "$INSTALL_ORG" -s "$INSTALL_SPACE"
+cf version
