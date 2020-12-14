f00a867b7105ca528aa77274b779d5c9374a441c
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-12-14 13:17:52 +0000

    Alternative install method.

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,6 +1,13 @@
 BASE_INSTALLER_URL="https://packages.cloudfoundry.org/stable?release=${CLOUDFOUNDRY_BINARY_PLATFORM}64-binary&source=github&version=v${CLOUDFOUNDRY_BINARY_VERSION}"
 echo "Downloading ${BASE_INSTALLER_URL}"
 curl -L $BASE_INSTALLER_URL | tar -zx
-mv cf /usr/local/bin
+
+if which sudo > /dev/null; then
+	sudo mv cf /usr/local/bin
+else
+	mkdir -p ~/bin
+	mv cf ~/bin
+	echo "export PATH=\"${PATH}:~/bin\"" >> $BASH_ENV
+fi
 
 cf version
